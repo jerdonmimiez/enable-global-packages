5e00caad5fd0e560f53dbefe8a733562387fce9e
Anton Denisneko <jerdonmimiez@yandex.ru>
2018-11-03 06:44:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -34,18 +34,25 @@ findGlobalPackages(function(err, dirs) {
     }
     return;
   }
+  
   // get all currently defined NODE_PATHs
   var nodePaths = process.env.NODE_PATH ? process.env.NODE_PATH.split(path.delimiter) : [];
+  
   // globalNodePaths will be a list of strings representing paths to globally available node_modules directories
   var globalNodePaths = uniq(dirs.map(function (dir) {
     return path.resolve(dir, "..");
   }));
+  
   // merge them together
   nodePaths = uniq(nodePaths.concat(globalNodePaths));
+  
   // expose them back to NODE_PATH env var
+  
   process.env.NODE_PATH = nodePaths.join(path.delimiter);
+  
   // refresh paths for node
   require("module").Module._initPaths();
+  
   // emit the ready event in case someone needs it
   eventEmitter.emit("ready", globalNodePaths);
 });
